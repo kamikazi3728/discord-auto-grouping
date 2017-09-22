3a8b58e3b945446e7f70d0d9f3330034405527b9
Reece Appling <kamikazi3728@users.noreply.github.com>
2017-09-22 15:46:24 -0400

    Update discord-auto-grouping.js
    updated to most recent version of the master branch

--- discord-auto-grouping.js
+++ discord-auto-grouping.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var customchannels = 0;
-var customchannellimit = 2; //set limit for number of custom channels, edit to your needs
-var customchannelnames = [];
+const token = '';
 
 // Connect and perform routine maintenance.
 client.on('ready', () => {
@@ -11,6 +9,68 @@ client.on('ready', () => {
 	// Set the online status.
 	client.user.setStatus('online');
 	
+	// Order the channels.
+	orderChannels();
+});
+
+// Trigger on VOICE_STATE_UPDATE events.
+client.on('voiceStateUpdate', (oldMember, member) => {
+	
+	// Check if the user entered a new channel.
+	if (member.voiceChannelID) {
+		const newChannel = member.guild.channels.get(member.voiceChannelID);
+		
+		// If the user entered a game channel (prefixed with a game controller unicode emoji), group them into their own channel.
+		if (newChannel.name.startsWith(String.fromCodePoint('0x1F3AE'))) {
+			newChannel.clone(String.fromCodePoint('0x2501') + ' Group', true)
+				.then(createdChannel => {
+					createdChannel.edit({
+							bitrate: 96000,
+							position: newChannel.position + 50,
+							userLimit: newChannel.userLimit
+						})
+						.then(createdChannel => {
+							member.setVoiceChannel(createdChannel)
+								.then(console.log('[' + new Date().toISOString() + '] Moved user "' + member.user.username + '#' + member.user.discriminator + '" (' + member.user.id + ') to ' + createdChannel.type + ' channel "' + createdChannel.name + '" (' + createdChannel.id + ') at position ' + createdChannel.position))
+								.catch(console.error);
+						})
+						.catch(console.error);
+				})
+				.catch(console.error);
+		}
+	}
+	
+	// Check if the user came from another channel.
+	if (oldMember.voiceChannelID) {
+		const oldChannel = oldMember.guild.channels.get(oldMember.voiceChannelID);
+		
+		// Delete the user's now empty temporary channel, if applicable.
+		if (oldChannel.name.startsWith(String.fromCodePoint('0x2501') + ' Group') && !oldChannel.members.array().length) {
+			oldChannel.delete()
+				.then(function() {
+					console.log('[' + new Date().toISOString() + '] Deleted ' + oldChannel.type + ' channel "' + oldChannel.name + '" (' + oldChannel.id + ')');
+				})
+				.catch(console.error);
+		}
+	}
+});
+
+// Reorder channels when one is created.
+client.on('channelCreate', function(channel){
+	if(!channel.name.startsWith(String.fromCodePoint('0x2501') + ' Group')){
+		orderChannels();
+	}
+});
+
+// Reorder channels when one is deleted.
+client.on('channelDelete', function(channel){
+	if(!channel.name.startsWith(String.fromCodePoint('0x2501') + ' Group')){
+		orderChannels();
+	}
+});
+
+// Function to reorder channels.
+function orderChannels(){
 	// Get a list of channels.
 	var channelsOrdered = client.channels.array().slice(0);
 	
@@ -35,89 +95,6 @@ client.on('ready', () => {
 			.catch(console.error);
 		currentPosition += 100;
 	});
-});
-
-function checkcustomchannels(channelname){
-	var temp = false;
-	for(i = 0; i<customchannelnames.length){
-		if(customchannelnames[i]==channelname){
-			temp = true;
-		}
-	}
-	return temp;
 }
 
-bot.on("message", msg => {
-    if(msg.channel.name=="general"){//change general to whatever chat channel is to be used with this bot, general is the default
-       	if (msg.content.startsWith("!newchannel")) {
-		var msgwords = msg.content.split(" ");
-        	if(customchannels<customchannellimit&&msgwords.length>1){
-			var newchannelname;
-			for (i = 1; i < msgwords.length-1; i++) { 
-    				newchannelname += msgwords[i];
-			}
-			if(newchannelname.length<=20||newchannelname.length<3){//can't make channels with too long or too short names, numbers editable
-				msg.guild.createChannel(newchannelname, 'voice')
-				customchannelnames.push(newchannelname);
-					.then(createdChannel => {
-						createdChannel.edit({bitrate: 96000, position: channelsOrdered[channelsOrdered.length].position + 100})
-						.then(createdChannel => {
-							msg.member.setVoiceChannel(createdChannel)
-							.then(console.log('[' + new Date().toISOString() + '] Moved user "' + member.user.username + '#' + member.user.discriminator + '" (' + member.user.id + ') to ' + createdChannel.type + ' channel "' + createdChannel.name + '" (' + createdChannel.id + ') at position ' + createdChannel.position))
-								.catch(console.error);
-						})
-						.catch(console.error);
-					})
-					.catch(console.error);
-			   }
-			else{
-				bot.sendMessage(msg.channel, "Sorry, I can't create that channel at the moment, the channel name was either too long or too short, try a different name");
-			}
-		   msg.channel.sendMessage("pong!");
-		   }
-		else{ //too many custom channels, notify user(s) new channel cannot be created
-			bot.sendMessage(msg.channel, "Sorry, I can't create that channel at the moment, I either have too many custom channels or you gave me an invalid channel name");
-		}
-    }
-}
-});
-
-// Trigger on VOICE_STATE_UPDATE events.
-client.on('voiceStateUpdate', (oldMember, member) => {
-	
-	// Check if the user entered a new channel.
-	if (member.voiceChannelID) {
-		const newChannel = member.guild.channels.get(member.voiceChannelID);
-		
-		// If the user entered a game channel (prefixed with a game controller icon), group them into their own channel.
-		if (newChannel.name.startsWith('🎮')) {
-			member.guild.createChannel('━ Group', 'voice')
-				.then(createdChannel => {
-					createdChannel.edit({bitrate: 96000, position: newChannel.position + 50})
-						.then(createdChannel => {
-							member.setVoiceChannel(createdChannel)
-								.then(console.log('[' + new Date().toISOString() + '] Moved user "' + member.user.username + '#' + member.user.discriminator + '" (' + member.user.id + ') to ' + createdChannel.type + ' channel "' + createdChannel.name + '" (' + createdChannel.id + ') at position ' + createdChannel.position))
-								.catch(console.error);
-						})
-						.catch(console.error);
-				})
-				.catch(console.error);
-		}
-	}
-	
-	// Check if the user came from another channel.
-	if (oldMember.voiceChannelID) {
-		const oldChannel = oldMember.guild.channels.get(oldMember.voiceChannelID);
-		
-		// Delete the user's now empty temporary channel, if applicable.
-		if (oldChannel.name.includes('Group') && !oldChannel.members.array().length||checkcustomchannels(oldChannel.name) && !oldChannel.members.array().length) {
-			oldChannel.delete()
-				.then(function() {
-					console.log('[' + new Date().toISOString() + '] Deleted ' + oldChannel.type + ' channel "' + oldChannel.name + '" (' + oldChannel.id + ')');
-				})
-				.catch(console.error);
-		}
-	}
-});
-
-client.login('');
+client.login(token);
